e3bd2879af12e49a0a7815ab8951960822a76d32
Gian Marco Sibilla <gm.sibilla@gmail.com>
2015-09-05 23:23:05 +0200

    Added custom base size for icons

--- create_ios_icons.sh
+++ create_ios_icons.sh
@@ -3,12 +3,8 @@
 # Create 3 copies of the given image with 32px, 64px, 96px size
 #
 
-TAB_SIZE_BASE=32
-TAB_SIZE_BIG=64
-TAB_SIZE_HUGE=96
-BAR_SIZE_BASE=32
-BAR_SIZE_BIG=64
-BAR_SIZE_HUGE=96
+TAB_SIZE=32
+BAR_SIZE=22
 output_dir='./'
 
 while getopts ":o:" opt; do
@@ -23,16 +19,33 @@ while getopts ":o:" opt; do
 	esac
 done
 
-shift $(($OPTIND - 2))
+shift $(($OPTIND - 1))
 
-if [[ $# < 1 ]]; then
-	echo "USAGE: $0 [-o OUTPUT_DIR] asset image"
+if [[ $# < 2 || $size_base && $# < 1 ]]; then
+	echo "USAGE: " + `basename $0` + " [-o OUTPUT_DIR] size image"
 	exit 1
 fi
 
-asset=$1
+size=$1
 image=$2
-echo "Converting $image to asset $asset (output directory: $output_dir)"
+re='^[0-9]+$'
+if [[ $size =~ $re ]]; then
+	size_base=$size;
+else
+	case "$size" in
+		tab) size_base=$TAB_SIZE
+			 ;;
+		bar) size_base=$BAR_SIZE
+			 ;;
+		*) echo "Unrecognized size $size. Available constant options are: tab, bar"
+		   exit 1
+		   ;;
+	esac
+fi
+size_big=$(($size_base * 2))
+size_huge=$(($size_base * 3))
+
+echo "Converting $image to size $size (output directory: $output_dir)"
 
 # Check if output_dir is a directory
 if [ ! -d "$output_dir" ]; then
@@ -40,31 +53,17 @@ if [ ! -d "$output_dir" ]; then
 	exit 1
 fi
 
-case "$asset" in
-	tab) $size_base = $TAB_SIZE_BASE
-		 $size_big = $TAB_SIZE_BIG
-		 $size_huge = $TAB_SIZE_HUGE
-		 ;;
-	bar) $size_base = $BAR_SIZE_BASE
-		 $size_big = $BAR_SIZE_BIG
-		 $size_huge = $BAR_SIZE_HUGE
-		 ;;
-	*) echo "Unrecognized asset $asset. Available options: tab, bar"
-	   exit 1
-	   ;;
-esac
-
 # Check image size
-for size in `sips -g pixelWidth -g pixelHeight $image | sed -n 's/.*pixel.*: \([0-9]*\)/\1/p'`; do
-	if [[ $size -lt $size_huge ]]; then
-		echo "Given image smallest side should be at least $size_huge px (currently $size px)"
+for side in `sips -g pixelWidth -g pixelHeight $image | sed -n 's/.*pixel.*: \([0-9]*\)/\1/p'`; do
+	if [[ $side -lt $size_huge ]]; then
+		echo "Given image smallest side should be at least $size_huge px (currently $side px)"
 		exit 1
 	fi
 done
 
 # Convert
 format=`sips -g format $image | sed -n 's/.*format: \([a-z]*\)/\1/p'`
-file_name=`echo $image | sed -n "s/\(.*\)\.$format/\1/p"`
+file_name=`basename $image | sed -n "s/\(.*\)\.$format/\1/p"`
 sips -Z $size_base -s format png $image --out $output_dir/$file_name".png"
 sips -Z $size_big -s format png $image --out $output_dir/$file_name"@2x.png"
 sips -Z $size_huge -s format png $image --out $output_dir/$file_name"@3x.png"
